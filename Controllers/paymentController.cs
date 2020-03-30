14d9659b6ff775c8a7bc19d11a1ce11addb0f072
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-03-30 12:23:37 -0600

    f1

---
+++ Controllers/paymentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaymentService.models;
+using Microsoft.OpenApi.Models;
+
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class paymentController : ControllerBase
+    {
+        // POST: api/paymentservice
+        [HttpPost]        
+        public string TransactionID(PaymentModel payment)
+        {
+            string transactionID = payment.TargetNumber + DateTime.Today.ToString("ddMMyyyy") + DateTime.Now.ToString("HHmmss");
+            return transactionID;
+        }
+        
+    }   
+}               
---
+++ Models/PaymentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PaymentService.models
+{
+    public class PaymentModel
+    {
+        public string TargetNumber { get; set; }
+        public double TotalToPay { get; set; }
+    }
+    
+}
